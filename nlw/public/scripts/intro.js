f5a1042667bc169818a90ce6a51c78e0a8ff5361
Yasmin <yasmin.s.luzia@gmail.com>
2020-06-09 15:20:17 -0300

    NextLevelWeek

---
+++ nlw/public/scripts/intro.js
@@ -0,0 +1,38 @@
+//variaveis
+// var myvar = "He"
+// let mylet = "He"
+// const myconst = "He"
+// document.write(myconst + myvar)
+
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr() {
+//     return "Run Forest, run!"
+//   }
+// }
+// document.write(pessoa.correr())
+
+// Array
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "MyName"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+//Funções
+// function sayMyName(name) {
+//   document.write(name)
+// }
+// sayMyName("Yasmin")
+
+// condicionais
+// const notaFinal = 7
+// if (notaFinal < 5) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p> ${i} </p>`)
+// }
